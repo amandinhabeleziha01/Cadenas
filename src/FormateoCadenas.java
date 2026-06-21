db753ab10a1f3a8722da62ce216277e718a1fb72
amandabatistadossantos <amandabatistadossantos90@gmail.com>
2026-06-20 21:48:55 -0500

    Manejo de Cadenas en Java

--- src/FormateoCadenas.java
+++ src/FormateoCadenas.java
@@ -17,6 +17,26 @@ public class FormateoCadenas {
                 edad,
                 salario);
 
+        // Formateo con text block
+        mensaje = """
+                %n Detalle Persona:\s
+                ------------------
+                \tNombre: %s
+                \tEdad: %d anos
+                \tSalario: $%.2f
+                """.formatted(nombre, edad, salario);
+        System.out.println("mensaje = " + mensaje);
+
+        // Formateo con tex black y printf diractamente
+        System.out.printf("""
+                %n Detalle Persona:\s
+                ------------------
+                \tNombre: %s
+                \tEdad: %d anos
+                \tSalario: $%.2f
+                """, nombre, edad, salario);
+
+
 
     }
 }
